09f34d27ef7122a8cd74ba7718d9d9452c6fab5a
Mateusz Myalski <mateusz.myalski@gmail.com>
2024-09-13 22:30:06 +0200

    Apply clang format

--- src/thinkpad_bat.c
+++ src/thinkpad_bat.c
@@ -19,10 +19,10 @@
 
 #include <assert.h>
 #include <fcntl.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 static unsigned bat_energy_full = 0;
 static unsigned battery_threshold = 0;
